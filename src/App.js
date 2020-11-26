638d202a753f14789588bea4a97513ba801b7292
Harsh Shukla <harsh.307.shuklaaa@gmail.com>
2020-11-26 20:37:20 +0530

    meet-app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Welcome from './components/Welcome/Welcome';
 import Chat from './components/Chat/chat';
 const App=()=>(
-    <Router>
+    <Router basename={window.location.pathname||''}>
         <Route path="/" exact component={Welcome}/>
         <Route path="/chat" component={Chat}/>
     </Router>
